ecd11fd7d7a76d32114d8ef9e32d87fbf037dfbb
unknown <lstradiot15@gmail.com>
2023-11-26 11:54:36 -0300

    Starting to do the aside and header responsive

--- src/Shared/Aside/index.js
+++ src/Shared/Aside/index.js
@@ -5,6 +5,11 @@ import styles from './aside.module.css';
 const Aside = ({ page }) => {
   const location = useLocation();
   const [activeButton, setActiveButton] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   useEffect(() => {
     const currentPath = location.pathname === '/' ? 'home' : location.pathname.substring(1);
@@ -16,52 +21,71 @@ const Aside = ({ page }) => {
       <aside className={styles.aside}>
         <div className={styles.asideSubContainer}>
           <img src="assets/logo.png" alt="logo-DS" className={styles.logo} />
-          <nav className={styles.navbar}>
+          <div className={styles.menuButton} onClick={toggleMenu}>
+            <div className={`${isOpen ? styles.x1 : styles.bar}`}></div>
+            <div className={`${isOpen ? styles.x2 : styles.bar} `}></div>
+            <div className={`${isOpen ? '' : styles.bar} `}></div>
+          </div>
+          <nav className={`${isOpen ? styles.activeMenu : styles.menu}`}>
             <ul className={styles.rutes}>
               {page === 'home' && (
                 <>
-                  <Link
-                    to="/login"
-                    className={`${activeButton === 'login' ? styles.activeBtn : styles.btn}`}
-                  >
-                    <li>Login</li>
-                  </Link>
-                  <Link
-                    to="/"
-                    className={`${activeButton === 'home' ? styles.activeBtn : styles.btn}`}
-                  >
-                    <li>Home</li>
-                  </Link>
-                  <Link
-                    to="/carreras"
-                    className={`${activeButton === 'carreras' ? styles.activeBtn : styles.btn}`}
-                  >
-                    <li>Carreras</li>
-                  </Link>
-                  <Link
-                    to="/inscripciones"
-                    className={`${
-                      activeButton === 'inscripciones' ? styles.activeBtn : styles.btn
-                    }`}
-                  >
-                    <li>Inscripciones</li>
-                  </Link>
+                  <li>
+                    <Link
+                      to="/login"
+                      className={`${activeButton === 'login' ? styles.activeBtn : styles.btn}`}
+                    >
+                      Login
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/"
+                      className={`${activeButton === 'home' ? styles.activeBtn : styles.btn}`}
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/carreras"
+                      className={`${activeButton === 'carreras' ? styles.activeBtn : styles.btn}`}
+                    >
+                      Carreras
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/inscripciones"
+                      className={`${
+                        activeButton === 'inscripciones' ? styles.activeBtn : styles.btn
+                      }`}
+                    >
+                      Inscripciones
+                    </Link>
+                  </li>
                 </>
               )}
               {page === 'super-admin' && (
                 <>
-                  <Link
-                    to="/super-admin"
-                    className={`${activeButton === 'super-admin' ? styles.activeBtn : styles.btn}`}
-                  >
-                    <li>Admin</li>
-                  </Link>
-                  <Link
-                    to="/"
-                    className={`${activeButton === 'home' ? styles.activeBtn : styles.btn}`}
-                  >
-                    <li>Home</li>
-                  </Link>
+                  <li>
+                    <Link
+                      to="/super-admin"
+                      className={`${
+                        activeButton === 'super-admin' ? styles.activeBtn : styles.btn
+                      }`}
+                    >
+                      Admin
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/"
+                      className={`${activeButton === 'home' ? styles.activeBtn : styles.btn}`}
+                    >
+                      Home
+                    </Link>
+                  </li>
                 </>
               )}
             </ul>
