ebd2bc0fa6863ee22a5f4ca9beec4d9e1243f273
unknown <lstradiot15@gmail.com>
2023-11-27 16:48:36 -0300

    Finished shared components and login view

--- src/Views/Login/index.js
+++ src/Views/Login/index.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './login.module.css';
 import { Link } from 'react-router-dom';
 import Aside from '../../Shared/Aside';
 import TextInput from '../../Shared/TextInput';
 import Button from '../../Shared/Button';
+import Modal from '../../Shared/Modal';
+
 const Login = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [responseModal, setResponseModal] = useState({
+    title: 'Eliminacion',
+    description: 'Desea rechazar al usuario?',
+    confirmBtn: 'Aceptar',
+    denyBtn: 'Cancelar'
+  });
+
+  const onSubmit = () => {
+    setResponseModal(responseModal);
+    setIsOpen(true);
+  };
+
+  const confirm = () => {
+    console.log('holis');
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
+      <Modal
+        title={responseModal.title}
+        description={responseModal.description}
+        confirmBtn={responseModal.confirmBtn}
+        denyBtn={responseModal.denyBtn}
+        chooseModal={responseModal}
+        isOpen={isOpen}
+        close={() => setIsOpen(!isOpen)}
+        onClick={() => confirm()}
+      />
       <Aside page={'home'} />
       <main>
         <section className={styles.container}>
@@ -14,14 +44,14 @@ const Login = () => {
             <form className={styles.loginContainer}>
               <TextInput
                 labelName={'E-mail'}
-                placeholderText={'Escribe aquí tu dirección de correo electrónico'}
+                placeholderText={'Escribe tu dirección de correo electrónico'}
               />
-              <TextInput labelName={'Contraseña'} placeholderText={'Escribe aquí tu contraseña'} />
+              <TextInput labelName={'Contraseña'} placeholderText={'Escribe tu contraseña'} />
               <Link to="/recoverPassword" className={styles.password}>
                 <p>Olvidaste tu contraseña?</p>
               </Link>
               <div className={styles.btnContainer}>
-                <Button type="submit" text="Enviar" />
+                <Button type="submit" text="Enviar" onClick={onSubmit} />
               </div>
             </form>
           </div>
