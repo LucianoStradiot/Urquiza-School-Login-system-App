3228f8e1ebc89b6e68a1500a4600aa95802748b9
unknown <lstradiot15@gmail.com>
2023-11-27 13:00:52 -0300

    Starting with shared button, text-input, modal and login page

--- src/Views/Login/index.js
+++ src/Views/Login/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import styles from './login.module.css';
+import { Link } from 'react-router-dom';
 import Aside from '../../Shared/Aside';
-
+import TextInput from '../../Shared/TextInput';
+import Button from '../../Shared/Button';
 const Login = () => {
   return (
     <>
       <Aside page={'home'} />
-      <div>Login</div>
+      <main>
+        <section className={styles.container}>
+          <div className={styles.subContainer}>
+            <form className={styles.loginContainer}>
+              <TextInput
+                labelName={'E-mail'}
+                placeholderText={'Escribe aquí tu dirección de correo electrónico'}
+              />
+              <TextInput labelName={'Contraseña'} placeholderText={'Escribe aquí tu contraseña'} />
+              <Link to="/recoverPassword" className={styles.password}>
+                <p>Olvidaste tu contraseña?</p>
+              </Link>
+              <div className={styles.btnContainer}>
+                <Button type="submit" text="Enviar" />
+              </div>
+            </form>
+          </div>
+        </section>
+      </main>
     </>
   );
 };
