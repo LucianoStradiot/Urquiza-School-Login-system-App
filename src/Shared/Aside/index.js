a6a3b28d3841694c4aedc65c74cff0f846d53085
unknown <lstradiot15@gmail.com>
2023-11-28 13:35:24 -0300

    Added the respectives users styles for the aside

--- src/Shared/Aside/index.js
+++ src/Shared/Aside/index.js
@@ -12,7 +12,7 @@ const Aside = ({ page }) => {
   };
 
   useEffect(() => {
-    const selectedRole = 'AfF';
+    const selectedRole = 'SUPER_ADMIN';
     sessionStorage.setItem('role', selectedRole);
   }, []);
 
@@ -191,41 +191,7 @@ const Aside = ({ page }) => {
             </div>
             <nav className={`${isOpen ? styles.activeMenu : styles.menu}`}>
               <ul className={styles.rutes}>
-                <li>
-                  <Link
-                    to="/login"
-                    className={`${activeButton === 'login' ? styles.activeBtn : styles.btn}`}
-                  >
-                    Login
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/"
-                    className={`${activeButton === 'home' ? styles.activeBtn : styles.btn}`}
-                  >
-                    Home
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/carreras"
-                    className={`${activeButton === 'carreras' ? styles.activeBtn : styles.btn}`}
-                  >
-                    Carreras
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/inscripciones"
-                    className={`${
-                      activeButton === 'inscripciones' ? styles.activeBtn : styles.btn
-                    }`}
-                  >
-                    Inscripciones
-                  </Link>
-                </li>
-                {page === 'super-admin' && (
+                {sessionStorage.getItem('role') === 'SUPER_ADMIN' ? (
                   <>
                     <li>
                       <Link
@@ -234,7 +200,41 @@ const Aside = ({ page }) => {
                           activeButton === 'super-admin' ? styles.activeBtn : styles.btn
                         }`}
                       >
-                        Admin
+                        Administración
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/carreras"
+                        className={`${activeButton === 'carreras' ? styles.activeBtn : styles.btn}`}
+                      >
+                        Carreras
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/inscripciones"
+                        className={`${
+                          activeButton === 'inscripciones' ? styles.activeBtn : styles.btn
+                        }`}
+                      >
+                        Inscripciones
+                      </Link>
+                    </li>
+                    <li>
+                      <a className={`${activeButton === 'logout' ? styles.activeBtn : styles.btn}`}>
+                        Logout
+                      </a>
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    <li>
+                      <Link
+                        to="/login"
+                        className={`${activeButton === 'login' ? styles.activeBtn : styles.btn}`}
+                      >
+                        Login
                       </Link>
                     </li>
                     <li>
@@ -245,6 +245,24 @@ const Aside = ({ page }) => {
                         Home
                       </Link>
                     </li>
+                    <li>
+                      <Link
+                        to="/carreras"
+                        className={`${activeButton === 'carreras' ? styles.activeBtn : styles.btn}`}
+                      >
+                        Carreras
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/inscripciones"
+                        className={`${
+                          activeButton === 'inscripciones' ? styles.activeBtn : styles.btn
+                        }`}
+                      >
+                        Inscripciones
+                      </Link>
+                    </li>
                   </>
                 )}
               </ul>
@@ -253,6 +271,37 @@ const Aside = ({ page }) => {
         </aside>
       </>
     )
+  ) : page === 'super-admin' ? (
+    <>
+      <aside className={styles.aside}>
+        <div className={styles.asideSubContainer}>
+          <img src="assets/logoLanding.png" alt="logo-DS" className={styles.logo} />
+          <div className={styles.title}>Escuela Superior de Comercio N°49</div>
+          <div className={styles.menuButton} onClick={toggleMenu}>
+            <div className={`${isOpen ? styles.x1 : styles.bar}`}></div>
+            <div className={`${isOpen ? styles.x2 : styles.bar} `}></div>
+            <div className={`${isOpen ? '' : styles.bar} `}></div>
+          </div>
+          <nav className={`${isOpen ? styles.activeMenu : styles.menu}`}>
+            <ul className={styles.rutes}>
+              <li>
+                <Link
+                  to="/"
+                  className={`${activeButton === 'home' ? styles.activeBtn : styles.btn}`}
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <a className={`${activeButton === 'logout' ? styles.activeBtn : styles.btn}`}>
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </aside>
+    </>
   ) : null;
 };
 
---
+++ src/Views/SuperAdmin/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Aside from '../../Shared/Aside';
+
+const SuperAdmin = () => {
+  return (
+    <>
+      <Aside page={'super-admin'} />
+      <div>SuperAdmin</div>
+    </>
+  );
+};
+
+export default SuperAdmin;
