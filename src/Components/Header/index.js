f75965be65011695764b9446747719d377f1d58e
unknown <lstradiot15@gmail.com>
2023-11-28 12:20:00 -0300

    Added future session storage for to change when a determined user logged

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './header.module.css';
 
 const Header = () => {
-  return (
+  useEffect(() => {
+    const selectedRole = 'AfF';
+    sessionStorage.setItem('role', selectedRole);
+  }, []);
+
+  return sessionStorage.getItem('role') === 'DS' ? (
+    <>
+      <header className={styles.header}>
+        <div className={`${styles.container} ${styles.containerDs}`}>
+          <h1 className={styles.title}>Escuela Superior de Comercio N°49</h1>
+        </div>
+        <div className={styles.wallpaper}></div>
+      </header>
+    </>
+  ) : sessionStorage.getItem('role') === 'AF' ? (
+    <>
+      <header className={styles.header}>
+        <div className={`${styles.container} ${styles.containerAf}`}>
+          <h1 className={styles.title}>Escuela Superior de Comercio N°49</h1>
+        </div>
+        <div className={styles.wallpaper}></div>
+      </header>
+    </>
+  ) : sessionStorage.getItem('role') === 'ITI' ? (
+    <>
+      <header className={styles.header}>
+        <div className={`${styles.container} ${styles.containerIti}`}>
+          <h1 className={styles.title}>Escuela Superior de Comercio N°49</h1>
+        </div>
+        <div className={styles.wallpaper}></div>
+      </header>
+    </>
+  ) : (
     <>
       <header className={styles.header}>
         <div className={styles.container}>
--- src/Shared/Aside/index.js
+++ src/Shared/Aside/index.js
@@ -12,89 +12,248 @@ const Aside = ({ page }) => {
   };
 
   useEffect(() => {
+    const selectedRole = 'AfF';
+    sessionStorage.setItem('role', selectedRole);
+  }, []);
+
+  useEffect(() => {
     const currentPath = location.pathname === '/' ? 'home' : location.pathname.substring(1);
     setActiveButton(currentPath);
   }, []);
 
-  return (
-    <>
-      <aside className={styles.aside}>
-        <div className={styles.asideSubContainer}>
-          <img src="assets/logo.png" alt="logo-DS" className={styles.logo} />
-          <div className={styles.title}>Escuela Superior de Comercio N°49</div>
-          <div className={styles.menuButton} onClick={toggleMenu}>
-            <div className={`${isOpen ? styles.x1 : styles.bar}`}></div>
-            <div className={`${isOpen ? styles.x2 : styles.bar} `}></div>
-            <div className={`${isOpen ? '' : styles.bar} `}></div>
+  return page === 'home' ? (
+    sessionStorage.getItem('role') === 'DS' ? (
+      <>
+        <aside className={`${styles.aside} ${styles.asideDs}`}>
+          <div className={styles.asideSubContainer}>
+            <img src="assets/logo.png" alt="logo-DS" className={styles.logo} />
+            <div className={styles.title}>Escuela Superior de Comercio N°49</div>
+            <div className={styles.menuButton} onClick={toggleMenu}>
+              <div className={`${isOpen ? styles.x1 : styles.bar}`}></div>
+              <div className={`${isOpen ? styles.x2 : styles.bar} `}></div>
+              <div className={`${isOpen ? '' : styles.bar} `}></div>
+            </div>
+            <nav className={`${isOpen ? styles.activeMenu : styles.menu}`}>
+              <ul className={styles.rutes}>
+                <li>
+                  <Link
+                    to="/login"
+                    className={`${activeButton === 'login' ? styles.activeBtn : styles.btn}`}
+                  >
+                    Login
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/"
+                    className={`${activeButton === 'home' ? styles.activeBtn : styles.btn}`}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/carreras"
+                    className={`${activeButton === 'carreras' ? styles.activeBtn : styles.btn}`}
+                  >
+                    Carreras
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/inscripciones"
+                    className={`${
+                      activeButton === 'inscripciones' ? styles.activeBtn : styles.btn
+                    }`}
+                  >
+                    Inscripciones
+                  </Link>
+                </li>
+              </ul>
+            </nav>
           </div>
-          <nav className={`${isOpen ? styles.activeMenu : styles.menu}`}>
-            <ul className={styles.rutes}>
-              {page === 'home' && (
-                <>
-                  <li>
-                    <Link
-                      to="/login"
-                      className={`${activeButton === 'login' ? styles.activeBtn : styles.btn}`}
-                    >
-                      Login
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/"
-                      className={`${activeButton === 'home' ? styles.activeBtn : styles.btn}`}
-                    >
-                      Home
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/carreras"
-                      className={`${activeButton === 'carreras' ? styles.activeBtn : styles.btn}`}
-                    >
-                      Carreras
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/inscripciones"
-                      className={`${
-                        activeButton === 'inscripciones' ? styles.activeBtn : styles.btn
-                      }`}
-                    >
-                      Inscripciones
-                    </Link>
-                  </li>
-                </>
-              )}
-              {page === 'super-admin' && (
-                <>
-                  <li>
-                    <Link
-                      to="/super-admin"
-                      className={`${
-                        activeButton === 'super-admin' ? styles.activeBtn : styles.btn
-                      }`}
-                    >
-                      Admin
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/"
-                      className={`${activeButton === 'home' ? styles.activeBtn : styles.btn}`}
-                    >
-                      Home
-                    </Link>
-                  </li>
-                </>
-              )}
-            </ul>
-          </nav>
-        </div>
-      </aside>
-    </>
-  );
+        </aside>
+      </>
+    ) : sessionStorage.getItem('role') === 'AF' ? (
+      <>
+        <aside className={`${styles.aside} ${styles.asideAf}`}>
+          <div className={styles.asideSubContainer}>
+            <img src="assets/logo.png" alt="logo-DS" className={styles.logo} />
+            <div className={styles.title}>Escuela Superior de Comercio N°49</div>
+            <div className={styles.menuButton} onClick={toggleMenu}>
+              <div className={`${isOpen ? styles.x1 : styles.bar}`}></div>
+              <div className={`${isOpen ? styles.x2 : styles.bar} `}></div>
+              <div className={`${isOpen ? '' : styles.bar} `}></div>
+            </div>
+            <nav className={`${isOpen ? styles.activeMenu : styles.menu}`}>
+              <ul className={styles.rutes}>
+                <li>
+                  <Link
+                    to="/login"
+                    className={`${activeButton === 'login' ? styles.activeBtn : styles.btn}`}
+                  >
+                    Login
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/"
+                    className={`${activeButton === 'home' ? styles.activeBtn : styles.btn}`}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/carreras"
+                    className={`${activeButton === 'carreras' ? styles.activeBtn : styles.btn}`}
+                  >
+                    Carreras
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/inscripciones"
+                    className={`${
+                      activeButton === 'inscripciones' ? styles.activeBtn : styles.btn
+                    }`}
+                  >
+                    Inscripciones
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </aside>
+      </>
+    ) : sessionStorage.getItem('role') === 'ITI' ? (
+      <>
+        <aside className={`${styles.aside} ${styles.asideIti}`}>
+          <div className={styles.asideSubContainer}>
+            <img src="assets/logo.png" alt="logo-DS" className={styles.logo} />
+            <div className={styles.title}>Escuela Superior de Comercio N°49</div>
+            <div className={styles.menuButton} onClick={toggleMenu}>
+              <div className={`${isOpen ? styles.x1 : styles.bar}`}></div>
+              <div className={`${isOpen ? styles.x2 : styles.bar} `}></div>
+              <div className={`${isOpen ? '' : styles.bar} `}></div>
+            </div>
+            <nav className={`${isOpen ? styles.activeMenu : styles.menu}`}>
+              <ul className={styles.rutes}>
+                <li>
+                  <Link
+                    to="/login"
+                    className={`${activeButton === 'login' ? styles.activeBtn : styles.btn}`}
+                  >
+                    Login
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/"
+                    className={`${activeButton === 'home' ? styles.activeBtn : styles.btn}`}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/carreras"
+                    className={`${activeButton === 'carreras' ? styles.activeBtn : styles.btn}`}
+                  >
+                    Carreras
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/inscripciones"
+                    className={`${
+                      activeButton === 'inscripciones' ? styles.activeBtn : styles.btn
+                    }`}
+                  >
+                    Inscripciones
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </aside>
+      </>
+    ) : (
+      <>
+        <aside className={styles.aside}>
+          <div className={styles.asideSubContainer}>
+            <img src="assets/logoLanding.png" alt="logo-DS" className={styles.logo} />
+            <div className={styles.title}>Escuela Superior de Comercio N°49</div>
+            <div className={styles.menuButton} onClick={toggleMenu}>
+              <div className={`${isOpen ? styles.x1 : styles.bar}`}></div>
+              <div className={`${isOpen ? styles.x2 : styles.bar} `}></div>
+              <div className={`${isOpen ? '' : styles.bar} `}></div>
+            </div>
+            <nav className={`${isOpen ? styles.activeMenu : styles.menu}`}>
+              <ul className={styles.rutes}>
+                <li>
+                  <Link
+                    to="/login"
+                    className={`${activeButton === 'login' ? styles.activeBtn : styles.btn}`}
+                  >
+                    Login
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/"
+                    className={`${activeButton === 'home' ? styles.activeBtn : styles.btn}`}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/carreras"
+                    className={`${activeButton === 'carreras' ? styles.activeBtn : styles.btn}`}
+                  >
+                    Carreras
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/inscripciones"
+                    className={`${
+                      activeButton === 'inscripciones' ? styles.activeBtn : styles.btn
+                    }`}
+                  >
+                    Inscripciones
+                  </Link>
+                </li>
+                {page === 'super-admin' && (
+                  <>
+                    <li>
+                      <Link
+                        to="/super-admin"
+                        className={`${
+                          activeButton === 'super-admin' ? styles.activeBtn : styles.btn
+                        }`}
+                      >
+                        Admin
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/"
+                        className={`${activeButton === 'home' ? styles.activeBtn : styles.btn}`}
+                      >
+                        Home
+                      </Link>
+                    </li>
+                  </>
+                )}
+              </ul>
+            </nav>
+          </div>
+        </aside>
+      </>
+    )
+  ) : null;
 };
 
 export default Aside;
