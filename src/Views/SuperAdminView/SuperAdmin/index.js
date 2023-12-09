de5696815bbbdae10112a0db330db0c1c39b91d6
unknown <lstradiot15@gmail.com>
2023-12-09 18:00:27 -0300

    Implement superadmin table with the back-end information

--- src/Views/SuperAdminView/SuperAdmin/index.js
+++ src/Views/SuperAdminView/SuperAdmin/index.js
@@ -8,18 +8,49 @@ import axiosClient from '../../../Components/Shared/Axios';
 import Spinner from '../../../Components/Shared/Spinner';
 
 const SuperAdmin = () => {
-  const { modalState, closeModal } = useModalContext();
+  const { openModal, modalState, closeModal } = useModalContext();
   const [isLoading, setIsLoading] = useState(false);
+  const [students, setStudents] = useState([]);
 
   const getStudents = async () => {
     setIsLoading(true);
     try {
-      const { data } = await axiosClient.get('/super-admin');
-      console.log(data);
+      const { data } = await axiosClient.get('/super-admin/administration');
+      setStudents(data.data);
     } catch (err) {
       throw new Error();
     }
     setIsLoading(false);
+  };
+
+  const onDelete = async (s) => {
+    console.log('Student object:', s);
+    const clickDelete = async () => {
+      setIsLoading(true);
+      try {
+        console.log('Deleting student with ID:', s.id);
+        await axiosClient.delete(`/super-admin/administration/${s.id}`);
+        getStudents();
+        openModal({
+          description: 'Estudiante rechazado con éxito'
+        });
+      } catch (err) {
+        if (err.response === 500)
+          openModal({
+            description: 'Ocurrió un error. Por favor inténtelo de nuevo'
+          });
+      }
+      setIsLoading(false);
+    };
+
+    openModal({
+      title: 'Eliminación',
+      description: '¿Está seguro que desea eliminar al usuario?',
+      confirmBtn: 'Aceptar',
+      denyBtn: 'Cancelar',
+      chooseModal: true,
+      onClick: clickDelete
+    });
   };
 
   useEffect(() => {
@@ -38,28 +69,30 @@ const SuperAdmin = () => {
           <table className={styles.contTable}>
             <thead className={styles.theadTable}>
               <tr>
-                <th className={`${styles.thTable} ${styles.headers} ${styles.borderLeft}`}>
-                  Email
-                </th>
+                <th className={styles.thTable}>id</th>
                 <th className={styles.thTable}>Nombre</th>
-                <th className={styles.thTable}>Apellido</th>
                 <th className={styles.thTable}>DNI</th>
+                <th className={styles.thTable}>Email</th>
                 <th className={styles.thTable}>Carrera</th>
+                <th className={styles.thTable}>Fecha de creación</th>
                 <th className={`${styles.thTable} ${styles.headers} ${styles.borderRight}`}></th>
               </tr>
             </thead>
             <tbody className={styles.tbody}>
-              <tr className={styles.rows}>
-                <td className={styles.thTable}>37930691@terciariourquiza.edu.ar</td>
-                <td className={styles.thTable}>Luciano</td>
-                <td className={styles.thTable}>Stradiot</td>
-                <td className={styles.thTable}>37930691</td>
-                <td className={styles.thTable}>Desarrollo de software</td>
-                <td className={styles.thTable}>
-                  <BiCheck className={styles.check} />
-                  <BiX className={styles.delete} />
-                </td>
-              </tr>
+              {students.map((s, index) => (
+                <tr key={index} className={styles.rows}>
+                  <td className={styles.thTable}>{s.id}</td>
+                  <td className={styles.thTable}>{s.name}</td>
+                  <td className={styles.thTable}>{s.dni}</td>
+                  <td className={styles.thTable}>{s.email}</td>
+                  <td className={styles.thTable}>{s.career}</td>
+                  <td className={styles.thTable}>{s.created_at}</td>
+                  <td className={styles.thTable}>
+                    <BiCheck className={styles.check} />
+                    <BiX onClick={() => onDelete(s)} className={styles.delete} />
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
